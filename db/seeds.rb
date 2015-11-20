ada45f001cf133d17148a3e50ce60c6197e2292f
Aleksei Maridashvili <aleksei.maridashvili@altoros.com>
2015-11-20 17:26:12 +0300

    List books with rating filter

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Book.create!(title: 'PP', rating: 5)
+Book.create!(title: 'EG', rating: 4)
--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ListingBooksTest < ActionDispatch::IntegrationTest
   setup do
     Book.create!(title: 'PP', rating: 5)
-    Book.create!(title: 'EG', rating: 5)
+    Book.create!(title: 'EG', rating: 4)
   end
 
   test 'listing books' do
@@ -12,5 +12,13 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
     assert_equal Book.count, JSON.parse(response.body).size
+  end
+
+  test 'lists top rated books' do
+    get '/books?rating=5'
+
+    assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+    assert_equal 1, JSON.parse(response.body).size
   end
 end
