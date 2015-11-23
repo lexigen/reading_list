34aa426417ea987528fd10f41f7f316e490bb56e
Aleksei Maridashvili <aleksei.maridashvili@altoros.com>
2015-11-23 17:20:57 +0300

    Added creation test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,4 @@
+# TODO: find about about location: book error
 class BooksController < ApplicationController
   def index
     books = Book.all
@@ -6,5 +7,20 @@ class BooksController < ApplicationController
       books = books.where(rating: rating)
     end
     render json: books, status: 200
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      render json: book, status: 201#, location: book
+    else
+      render json: book.errors, status: 422
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :rating)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ActiveRecord::Base
   scope :finished, -> { where.not(finished_at: nil) }
+
+  validates :title, presence: true
 end
