f785d505dc827749499508e1cbbc20d8355a3c4e
Aleksei Maridashvili <aleksei.maridashvili@altoros.com>
2015-11-24 13:57:13 +0300

    Added genre

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  class Api::GenresController < ApplicationController
+    def index
+      genres = Genre.all
+
+      render json: genres, status: 200
+    end
+
+  end
+end
--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,3 +1,3 @@
-class BooksSerializer < ActiveModel::Serializer
-  attributes :id, :title, :rating
+class BookSerializer < ActiveModel::Serializer
+  attributes :id, :title, :rating, :author, :genre_id, :review, :amazon_id
 end
